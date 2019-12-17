3b7f7807e8f42639f468089dbca34834f5323864
Martin Stelmach <martinstelmach@hotmail.com>
2019-12-16 23:09:59 -0500

    Update stackeddensityshiny.R

--- finalvisualizations/stackeddensityshiny.R
+++ finalvisualizations/stackeddensityshiny.R
@@ -6,17 +6,17 @@ library(lubridate)
 library(RColorBrewer)
 library(imputeTS)
 
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
-               "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-
+#palette
+cbPalette <- rev(sequential_hcl(7, 'BurgYl'))
+#Name all the stages, provinces, labels and such
 stages <- c('L2o', 'L2', 'L3', 'L4', 'L5', 'L6')
 prov.names <- c('in', 'qc', 'nb', 'on', 'ip')
-prov.labs <- c('Northwest Territories', 'Quebec', 
-               'New Brunswick', 'Ontario', 'Lab-Reared')
+prov.labs <- c('N.W.T', 'QC', 
+               'N.B', 'ON', 'Lab-Reared')
 names(prov.labs) <- c(prov.names)
 
 sims.all.df <-readr::read_csv('https://raw.githubusercontent.com/stelmacm/budwormvisualizationStats744/master/datasets/sd_data.csv')
-
+#Bringing it all together and seperate the data as needed
 sadf.tt.lst <- lapply(1:nrow(sims.all.df), function(x) {
   row <- sims.all.df[x,]
   d <- as.character(row$date)
@@ -31,15 +31,15 @@ sadf.tt.lst <- lapply(1:nrow(sims.all.df), function(x) {
 })
 sadf.tt <- do.call('rbind', sadf.tt.lst)
 sadf.tt[,c(stages, 'Pupa')] <- round(sadf.tt[,c(stages, 'Pupa')], 2)
-
+#Factor everything 
 names(sadf.tt)[1] <- 'Date'
 sadf.tt$Province <- toupper(sadf.tt$prov)
 sadf.tt$prov <- factor(sadf.tt$prov, c('nb', 'on', 'qc', 'in'))
 sadf.tt$stage <- factor(sadf.tt$stage, levels = c(stages, 'Pupa'))
 sadf.tt$Date <- as.Date(sadf.tt$Date)
-
+#Create a user interface where user selects year
 ui <- fluidPage(
-  titlePanel("Stacked Density Plot for the Inputted Year"),
+  titlePanel("Simulated Spruce Budworm Development by Location"),
   
   sidebarLayout(
     sidebarPanel(
@@ -55,13 +55,13 @@ ui <- fluidPage(
 
 server <- function(input, output){
   
-  
+  #Created the graph
   output$stacked <- renderPlotly({
     g <- ggplot(data=subset(sadf.tt, year == input$year), 
                 aes(x=Date, y = proportion, group=stage, fill=stage,
                     lab1 = L2o, lab2 = L2, lab3 = L3, lab4 = L4,
                     lab5 = L5, lab6 = L6, lab7 = Pupa, lab8 = Province)) +
-      scale_fill_manual(values = cbPalette[c(7, 2, 5, 4, 3, 6, 8)]) +
+      scale_fill_manual(values = cbPalette) +
       geom_density(position="fill", stat = 'identity', lwd = 0.05) +
       theme_minimal() +
       theme(panel.grid.major = element_blank(), 
@@ -72,7 +72,7 @@ server <- function(input, output){
       facet_grid(prov~., scales = 'free_x',  
                  labeller = labeller(prov = prov.labs)) +
       labs(y = 'Proportion of Population in Stage', 
-           fill = 'Larval Stage', x = 'Date', 
+           fill = 'Stage', x = 'Date', 
            title = 'Spruce Budworm Development by Location') +
       guides(alpha = FALSE)
     
@@ -85,4 +85,3 @@ server <- function(input, output){
 }
 
 shinyApp(ui, server)
-
